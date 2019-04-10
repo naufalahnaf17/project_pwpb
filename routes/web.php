0adc483cc0eff0b6fabdd09b8025e668bd30782c
vikoms <vikoms95@gmail.com>
2019-04-10 08:09:45 +0700

    yeuh register

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/signup/create', 'UserController@SignUp');
+Route::post('/signup', 'UserController@postSignUp');
+
 Route::get('/index', function () {
     return view('home.home');
 });
